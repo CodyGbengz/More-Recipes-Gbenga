7229d7eec1f40d99b4728f16d89b9eea8fb8d0dc
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-08-26 01:16:05 +0100

    Modify favourite recipes page

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -2,4 +2,5 @@
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     $(".button-collapse").sideNav();
+    $('.tooltipped').tooltip({delay: 100});
   });
