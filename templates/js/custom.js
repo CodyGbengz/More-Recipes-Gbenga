9e1fd66534eab75b571e95a453b2384251925f23
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-08-27 15:13:23 +0100

    Finish Home page template

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -2,5 +2,23 @@
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     $(".button-collapse").sideNav();
-    $('.tooltipped').tooltip({delay: 100});
+    $('.tooltipped').tooltip({delay: 500});
+    
   });
+ 
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTop").style.display = "block";
+    } else {
+        document.getElementById("backToTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+  
+}
