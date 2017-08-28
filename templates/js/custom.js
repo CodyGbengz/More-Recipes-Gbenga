5c1c00364b3a2fc471b0bc2d79df1baa07fddab1
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-08-28 13:40:48 +0100

    Make navbar links active

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -3,6 +3,14 @@
     $('.modal').modal();
     $(".button-collapse").sideNav();
     $('.tooltipped').tooltip({delay: 500});
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        hover: true, // Activate on hover
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      }
+    );
     
   });
  
