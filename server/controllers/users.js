3651b76474c28366f7d9e13b500f7033cb55f897
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-08-30 11:42:31 +0100

    implement sign up validation

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,6 +2,21 @@ import models from '../models';
 
 export default {
   create(req, res) {
+    // validate username ,email and password
+
+    if (!/\w{8}/.test(req.body.username)) {
+      return res.status(402).send({
+        message: 'Enter a username with atleast 8 characters'
+      });
+    } else if (!/^[\w\.-_\+]+@[\w-]+(\.\w{2,4})+$/.test(req.body.email)) {
+      return res.status(402).send({
+        message: 'Please enter a valid email'
+      });
+    } else if (!/^\w{8,12}$/.test(req.body.password)) {
+      return res.status(402).send({
+        message: 'Please Enter a password with atleast 8 characters'
+      });
+    }
     return models.Users
       .create({
         username: req.body.username,
