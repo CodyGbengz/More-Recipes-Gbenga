14a5a13588cfbf230068763339a3a33d2671fcfc
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-08-30 08:56:53 +0100

    create user signin controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,6 @@
+import md5 from 'md5';
 import models from '../models';
+
 
 export default {
   create(req, res) {
@@ -6,7 +8,7 @@ export default {
       .create({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: md5(req.body.password)
       })
       .then(user => res.status(201).send({
         username: user.username,
@@ -16,5 +18,27 @@ export default {
       .catch(error => res.status(400).send({
         error: error.message
       }));
+  },
+
+  signin(req, res) {
+    return models.Users
+      .findOne({ where:
+        { email: req.body.email,
+          password: md5(req.body.password) } })
+      .then((user) => {
+        if (!user) {
+          return res.status(401).send({
+            message: 'Invalid Username or Password'
+          });
+        }
+        req.session.user = user;
+        res.status(202).send({
+          username: user.username,
+          email: user.email,
+          message: 'Sign in successful'
+        })
+          .catch(error => res.status(400).send({
+            message: error.message }));
+      });
   }
 };
