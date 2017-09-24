e43b7f5ac0d13b732836e2e946e3cbacb47e91af
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-09-24 06:20:18 +0100

    implement add favorite category feature

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -6,10 +6,16 @@ import controllers from '../controllers/index';
 const router = express.Router();
 
 // route for adding to favorites
-router.post('/api/users/:userId/recipes', auth, controllers.Favorite.add);
+router.post('/api/users/:recipeId/favorites', auth, controllers.Favorite.add);
 
 // route for getting favorites
-router.get('/api/users/recipes', auth, controllers.Favorite.fetchUserFavorites);
+router.get('/api/users/favorites', auth, controllers.Favorite.fetchUserFavorites);
+
+// route for adding favorites to category
+router.put('/api/users/:recipeId/favorites', auth, controllers.Favorite.addToCategory);
+
+// route for deleting a recipe from favorites
+router.delete('/api/users/:recipeId/favorites', auth, controllers.Favorite.deleteUserFavourite);
 
 
 export default router;
