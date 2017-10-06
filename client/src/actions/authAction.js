088c08db2a67e99513193a6c1aad9d65b1a55fb9
Gbenga <odemichaelgbenga@gmail.com>
2017-10-06 15:37:36 +0100

    sign in user and save jwt on client

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwtDecode from 'jwt-decode';
+import { SET_CURRENT_USER } from './types';
+
+export function setCurrentUser(user) {
+    return {
+        type: SET_CURRENT_USER,
+        user
+    };
+}
 
 export function login(userData) {
     return dispatch => {
-        return axios.post('/api/users/signin', userData)
+        return axios.post('/api/users/signin', userData).then(res => {
+            const token = res.data.Token;
+            localStorage.setItem('jwtToken', token);
+            setAuthToken(token);
+            dispatch(setCurrentUser(jwtDecode(token)));
+        });
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,30 @@
+import jwtDecode from 'jwt-decode';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './rootReducer';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authAction';
+
 
 import './index.scss';
 import routes from './routes';
 
 const store = createStore(
-    (state = {}) => state,
-    applyMiddleware(thunk)
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
 );
+
+if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+}
 
 render(
     <Provider store={store}>
