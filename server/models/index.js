ffb6d19d4be84d81750eed78823d40d129f2a868
Gbenga <odemichaelgbenga@gmail.com>
2018-01-22 16:43:42 +0100

    fix production database setting

--- server/models/index.js
+++ server/models/index.js
@@ -12,7 +12,8 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable);
+  console.log(process.env[config.use_env_variable])
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
